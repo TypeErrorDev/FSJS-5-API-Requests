f11a9c18cb4d2a7bae19aa52a7cf36c2851d8b5b
TypeErrorDev <matthew.pantel86@gmail.com>
2022-08-15 23:32:55 +0300

    REMIX: converted css to scss....LFG!!!

--- js/app.js
+++ js/app.js
@@ -2,57 +2,29 @@
             GLOBAL VARIABLES
 ===================================== */
 const CARD = document.getElementsByClassName(".card");
+const SEARCH = document.getElementById("search-container");
+let urlAPI = `https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=US`;
+let overlay;
+/* ==================================
+        ASYNC/AWAIT CALLS
+===================================== */
 
 /* ==================================
-        FETCHING/POPULATING DATA
+        GENERATE CARDS HTML
 ===================================== */
-fetch("https://randomuser.me/api/?results=12&nat=us")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    const employees = data.results;
-    console.log(employees);
-    employees.forEach((employee) => {
-      console.log(employee);
-      const card = document.createElement("div");
-      card.className = "card";
-      card.innerHTML = `
-                <div class="card-img-container">
-                    <img class="card-img" src="${employee.picture.large}" alt="profile picture">
-                </div>
-                <div class="card-info-container">
-                    <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
-                    <p class="card-text">${employee.email}</p>
-                    <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
-                </div>
-            `;
-      document.querySelector("#gallery").appendChild(card);
-    });
-  });
+
+/* ==================================
+        GENERATE SEARCH HTML
+===================================== */
+
+/* ==================================
+        GENERATE MODAL HTML
+===================================== */
 
 /* ==================================
             EVENT LISTENERS
 ===================================== */
-document.addEventListener("click", (e) => {
-  if (e.target.className === "card") {
-    console.log("card clicked");
-  }
-});
 
 /* ==================================
-         SEARCH FILTER FUNCTION
+            EVENT LISTENERS
 ===================================== */
-function searchFilter() {
-  const input = document.getElementById("search-input");
-  const filter = input.value.toUpperCase();
-  const cards = document.getElementsByClassName("card");
-  const names = document.getElementsByClassName("card-name");
-  for (let i = 0; i < cards.length; i++) {
-    const name = names[i].textContent;
-    if (name.toUpperCase().indexOf(filter) > -1) {
-      cards[i].style.display = "";
-    } else {
-      cards[i].style.display = "none";
-    }
-  }
-}
